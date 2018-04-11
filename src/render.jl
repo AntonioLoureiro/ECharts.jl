632b00e6632b9516a27016b35807eff610a49f5d
AntonioLoureiro <ajose.loureiro@gmail.com>
2018-04-11 12:35:31 +0100

    Update render.jl

--- src/render.jl
+++ src/render.jl
@@ -69,3 +69,32 @@ function html(ec::EChart)
 
               """
 end
+
+function html(ec::EChart;public=false)
+
+    divid = "Echart" * randstring(10)
+    option = json(ec.options)
+    width = ec.width
+    height = ec.height
+
+
+        return """
+    
+                <div id=\"$divid\" style=\"height:$(height)px;width:$(width)px;\"></div>
+              
+                <script src=\"https://cdnjs.cloudflare.com/ajax/libs/echarts/3.8.5/echarts-en.min.js\">
+    
+                <script type=\"text/javascript\">
+
+                        // Initialize after dom ready
+                        var myChart = echarts.init(document.getElementById(\"$divid\"));
+
+                        // Load data into the ECharts instance
+                        myChart.setOption($option);
+
+
+                </script>
+
+              """
+end
+
